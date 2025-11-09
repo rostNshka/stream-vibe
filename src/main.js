390126bfacfa1e4bffb8a870ad0ee61f44663750
RostNshka <devkotder@ya.ru>
2025-11-09 18:56:08 +0400

    add script in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import OverlayMenu from '@/modules/OverlayMenu.js';
-import SliderCollection from "@/modules/SliderCollection";
+import OverlayMenu from '@/modules/OverlayMenu'
+import SliderCollection from '@/modules/SliderCollection'
+import TabsCollection from '@/modules/TabsCollection'
 
-new OverlayMenu();
-new SliderCollection();
+new OverlayMenu()
+new SliderCollection()
+new TabsCollection()
