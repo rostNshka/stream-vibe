4b9bcd169dcf17f5194530edaef9a737cc7b8a97
RostNshka <devkotder@ya.ru>
2025-11-15 12:59:42 +0400

    add new page

---
+++ src/pages/support.jsx
@@ -0,0 +1,14 @@
+import Support from "@/sections/Support/index.js";
+
+export const metadata = {
+  title: 'Support'
+}
+
+
+export default function () {
+  return (
+    <>
+      <Support />
+    </>
+  )
+}
