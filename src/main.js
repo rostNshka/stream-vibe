21e17347a8b8f59154e9431a13e98b445277f12a
RostNshka <devkotder@ya.ru>
2025-11-16 21:16:06 +0400

    add mask for Number label

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import OverlayMenu from '@/modules/OverlayMenu'
 import SliderCollection from '@/modules/SliderCollection'
 import TabsCollection from '@/modules/TabsCollection'
+import inputMaskCollection from "@/modules/inputMaskCollection.js";
 
 new OverlayMenu()
 new SliderCollection()
 new TabsCollection()
+new inputMaskCollection()
