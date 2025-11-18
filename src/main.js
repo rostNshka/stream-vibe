8db2ffd5616cb359a8fd3b4ff7584406504cbfdb
RostNshka <devkotder@ya.ru>
2025-11-18 19:16:47 +0400

    add new script in  main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import OverlayMenu from '@/modules/OverlayMenu'
 import SliderCollection from '@/modules/SliderCollection'
 import TabsCollection from '@/modules/TabsCollection'
 import inputMaskCollection from "@/modules/inputMaskCollection.js";
+import SelectCollection from "@/modules/SelectCollection.js";
 
 new OverlayMenu()
 new SliderCollection()
 new TabsCollection()
 new inputMaskCollection()
+new SelectCollection()
